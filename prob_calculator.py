8dcec1f18d8c77984033ccbb28de2b56437dc1eb
dtisoy <danieltisoy2001@gmail.com>
2023-06-02 16:44:52 -0500

    better for reading

--- prob_calculator.py
+++ prob_calculator.py
@@ -19,10 +19,12 @@ class Hat:
         c_contents = copy.copy(self.contents)
         if n_draw > len(self.contents):
             return c_contents
+        # create a set of index to delete
         to_del = set(random.sample(range(len(c_contents)), n_draw))
-        result = [item for index, item in enumerate(c_contents) if index in to_del]
+        # idx stands for index
+        result = [item for idx, item in enumerate(c_contents) if idx in to_del]
         self.contents = [
-            item for index, item in enumerate(c_contents) if not index in to_del
+            item for idx, item in enumerate(c_contents) if idx not in to_del
         ]
         return sorted(result)
 
@@ -32,15 +34,17 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     m = 0
     while i > 0:
         c_hat = copy.copy(hat)
-        # draw balls
+        # draw balls and count the result
         balls_drawn = Counter(c_hat.draw(num_balls_drawn))
+        # check if there are enough balls in the current draw
         enough = {
-            k: expected_balls[k]
-            for k in expected_balls
-            if k in balls_drawn and expected_balls[k] <= balls_drawn[k]
+            key: value
+            for key, value in expected_balls.items()
+            if key in balls_drawn and value <= balls_drawn[key]
         }
         # check if is correct:
         if len(enough) == len(expected_balls):
             m += 1
         i -= 1
-    return m / num_experiments
+    probability = m / num_experiments
+    return probability
